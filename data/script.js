9038bdf4c318ab0457d714c573f065a2353f2f9a
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-04-18 17:01:58 -0300

    basic setting in js

---
+++ data/script.js
@@ -0,0 +1,39 @@
+function modificar1(){
+	am = document.getElementById("am");
+	am.innerHTML = "A";
+};
+
+function demodificar1(){
+	am = document.getElementById("am");
+	am.innerHTML = ".-";
+};
+
+function modificar2(){
+	bm = document.getElementById("bm");
+	bm.innerHTML = "B";
+};
+
+function demodificar2(){
+	bm = document.getElementById("bm");
+	bm.innerHTML = "-...";
+};
+
+function modificar3(){
+	dm = document.getElementById("cm");
+	dm.innerHTML = "C";
+};
+
+function demodificar3(){
+	cm = document.getElementById("cm");
+	cm.innerHTML = "-.-.";
+};
+
+function modificar4(){
+	dm = document.getElementById("dm");
+	dm.innerHTML = "D";
+};
+
+function demodificar4(){
+	dm = document.getElementById("dm");
+	dm.innerHTML = "-.."
+};
