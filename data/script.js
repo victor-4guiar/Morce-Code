1851f65c15df8c20fd50d556a19bf4e19811b6ca
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-04-19 18:04:12 -0300

    Morce Alphabet Translator

--- data/script.js
+++ data/script.js
@@ -1,29 +1,29 @@
-function modificar1(){
+function modificar1(){ /* A = .- */
 	am = document.getElementById("am");
 	am.innerHTML = "A";
 };
 
-function demodificar1(){
+function demodificar1(){ /* A = .- */
 	am = document.getElementById("am");
 	am.innerHTML = ".-";
 };
 
-function modificar2(){
+function modificar2(){ /* B = -... */
 	bm = document.getElementById("bm");
 	bm.innerHTML = "B";
 };
 
-function demodificar2(){
+function demodificar2(){ /* B = -... */
 	bm = document.getElementById("bm");
 	bm.innerHTML = "-...";
 };
 
-function modificar3(){
+function modificar3(){ /* C = -.-. */
 	dm = document.getElementById("cm");
-	dm.innerHTML = "C";
+	dm.innerHTML = " C";
 };
 
-function demodificar3(){
+function demodificar3(){ /* C = -.-. */
 	cm = document.getElementById("cm");
 	cm.innerHTML = "-.-.";
 };
@@ -36,4 +36,224 @@ function modificar4(){
 function demodificar4(){
 	dm = document.getElementById("dm");
 	dm.innerHTML = "-.."
+};
+
+function modificar5(){
+	am = document.getElementById("em");
+	am.innerHTML = "E";
+};
+
+function demodificar5(){
+	am = document.getElementById("em");
+	am.innerHTML = ".";
+};
+
+function modificar6(){
+	bm = document.getElementById("fm");
+	bm.innerHTML = "F";
+};
+
+function demodificar6(){
+	bm = document.getElementById("fm");
+	bm.innerHTML = "..-.";
+};
+
+function modificar7(){
+	dm = document.getElementById("gm");
+	dm.innerHTML = "G";
+};
+
+function demodificar7(){
+	cm = document.getElementById("gm");
+	cm.innerHTML = "--.";
+};
+
+function modificar8(){
+	dm = document.getElementById("hm");
+	dm.innerHTML = "H";
+};
+
+function demodificar8(){
+	dm = document.getElementById("hm");
+	dm.innerHTML = "...."
+};
+
+function modificar9(){
+	am = document.getElementById("im");
+	am.innerHTML = "I";
+};
+
+function demodificar9(){
+	am = document.getElementById("im");
+	am.innerHTML = "..";
+};
+
+function modificar10(){
+	bm = document.getElementById("jm");
+	bm.innerHTML = "J";
+};
+
+function demodificar10(){
+	bm = document.getElementById("jm");
+	bm.innerHTML = ".---";
+};
+
+function modificar11(){
+	dm = document.getElementById("km");
+	dm.innerHTML = "K";
+};
+
+function demodificar11(){
+	cm = document.getElementById("km");
+	cm.innerHTML = "-.-";
+};
+
+function modificar12(){
+	dm = document.getElementById("lm");
+	dm.innerHTML = "L";
+};
+
+function demodificar12(){
+	dm = document.getElementById("lm");
+	dm.innerHTML = ".-.."
+};
+
+function modificar13(){
+	am = document.getElementById("mm");
+	am.innerHTML = "M";
+};
+
+function demodificar13(){
+	am = document.getElementById("mm");
+	am.innerHTML = "--";
+};
+
+function modificar14(){
+	bm = document.getElementById("nm");
+	bm.innerHTML = "N";
+};
+
+function demodificar14(){
+	bm = document.getElementById("nm");
+	bm.innerHTML = "-.";
+};
+
+function modificar15(){
+	dm = document.getElementById("om");
+	dm.innerHTML = "O";
+};
+
+function demodificar15(){
+	cm = document.getElementById("om");
+	cm.innerHTML = "---";
+};
+
+function modificar16(){
+	dm = document.getElementById("pm");
+	dm.innerHTML = "P";
+};
+
+function demodificar16(){
+	dm = document.getElementById("pm");
+	dm.innerHTML = ".--."
+};
+
+function modificar17(){
+	dm = document.getElementById("qm");
+	dm.innerHTML = "Q";
+};
+
+function demodificar17(){
+	cm = document.getElementById("qm");
+	cm.innerHTML = "--.-";
+};
+
+function modificar18(){
+	dm = document.getElementById("rm");
+	dm.innerHTML = "R";
+};
+
+function demodificar18(){
+	dm = document.getElementById("rm");
+	dm.innerHTML = ".-."
+};
+
+function modificar19(){
+	am = document.getElementById("sm");
+	am.innerHTML = "S";
+};
+
+function demodificar19(){
+	am = document.getElementById("sm");
+	am.innerHTML = "...";
+};
+
+function modificar20(){
+	bm = document.getElementById("tm");
+	bm.innerHTML = "T";
+};
+
+function demodificar20(){
+	bm = document.getElementById("tm");
+	bm.innerHTML = "-";
+};
+
+function modificar21(){
+	dm = document.getElementById("um");
+	dm.innerHTML = "U";
+};
+
+function demodificar21(){
+	cm = document.getElementById("um");
+	cm.innerHTML = "..-";
+};
+
+function modificar22(){
+	dm = document.getElementById("vm");
+	dm.innerHTML = "V";
+};
+
+function demodificar22(){
+	dm = document.getElementById("vm");
+	dm.innerHTML = "...-"
+};
+
+function modificar23(){
+	dm = document.getElementById("wm");
+	dm.innerHTML = "W";
+};
+
+function demodificar23(){
+	cm = document.getElementById("wm");
+	cm.innerHTML = ".--";
+};
+
+function modificar24(){
+	dm = document.getElementById("xm");
+	dm.innerHTML = "X";
+};
+
+function demodificar24(){
+	dm = document.getElementById("xm");
+	dm.innerHTML = "-..-"
+};
+
+function modificar25(){
+	dm = document.getElementById("ym");
+	dm.innerHTML = "Y";
+};
+
+function demodificar25(){
+	cm = document.getElementById("ym");
+	cm.innerHTML = "-.--";
+};
+
+function modificar26(){
+	dm = document.getElementById("zm");
+	dm.innerHTML = "Z";
+};
+
+function demodificar26(){
+	dm = document.getElementById("zm");
+	dm.innerHTML = "--.."
 };
